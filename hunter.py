1fa5050763cd22abdac2d71e799e10c4dbb4c29d
georkings <georkings88@gmail.com>
2021-10-05 03:48:11 -0400

    modify data input method to inline arguments

--- hunter.py
+++ hunter.py
@@ -1,14 +1,16 @@
 import tuenvioApi
 import os
+import sys
 from threading import Thread
 from time import sleep
 
 # tuenvioApi.addMethod = input("POST(p) o GET(g): ")
 tuenvioApi.addMethod = 'p'
-tuenvioApi.shopIndex = int(input("1. Pinar del Río\n2. Artemisa\n3. La Habana\n4. Carlos III\n5. El Pedregal\n6. Villa Diana\n7. Almacén Caribe\n8. Mayabeque\n9. Matanzas\
-      \n10. Cienfuegos\n11. Villa Clara\n12. Sancti Spíritus\n13. Ciego de Avila\n14. Camaguey\n15. Las Tunas\n16. Holguín\n17. Granma\n18. Santiago de Cuba\
-      \n19. Santiago de Cuba (Caribe)\n20. Guantánamo\n21. Isla de la Juventud\nInserte número de tienda: ")) - 1
-tuenvioApi.username = input("Usuario: ")
+if len(sys.argv) != 3:
+    print('Debe usar el sgte formato: "python hunter.py <tienda> <usuario>"')
+    sys.exit(0)
+tuenvioApi.shopIndex = tuenvioApi.shopList.index(sys.argv[1])
+tuenvioApi.username = sys.argv[2]
 
 if os.path.isdir('users') and os.path.isfile('users/' + tuenvioApi.username):
     file = open('users/' + tuenvioApi.username, 'r')
